0223e7b136a699ef7a7825d2c1bc6b49d95a9c1c
vnpugh <vnp19@yahoo.com>
2024-03-16 02:27:16 -0500

    updating user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ApplicationRecord
   # email uniqueness
   before_save { self.email = email.downcase }
 
+  has_many :watchlists, dependent: :destroy
+
   # Validations
   validates :firstname, presence: true, length: { maximum: 50 }
   validates :lastname, presence: true, length: { maximum: 50 }
@@ -12,6 +14,8 @@ class User < ApplicationRecord
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
 
+  validate :validate_watchlists_count       
+
   # bcrypt to hash passwords
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }
@@ -19,5 +23,11 @@ class User < ApplicationRecord
   # Method returns a greeting with the user's first name
   def greeting
     "Hello, #{firstname}!"
+  end
+
+  private
+
+  def validate_watchlists_count
+    errors.add(:watchlists, "exceeds maximum number of 10") if watchlists.count > 10
   end
 end
