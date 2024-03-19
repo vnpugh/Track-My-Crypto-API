70fd895b4914e8270b8f50c70b21f37755999bb1
vnpugh <vnp19@yahoo.com>
2024-03-19 01:06:14 -0500

    updated cors.rb

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,12 +1,11 @@
 # config/initializers/cors.rb
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
-    allow do
-      origins 'http://localhost:4200', # 'https://example.com' update with deployed address
-      resource '*',
-        headers: :any,
-        methods: [:get, :post, :put, :patch, :delete, :options, :head],
-        expose: ['Authorization'], 
-        max_age: 600 
-    end
+  allow do
+    origins 'http://localhost:4200' #, 'https://example.com' update with deployed address if needed
+    resource '*',
+      headers: :any,
+      methods: [:get, :post, :put, :patch, :delete, :options, :head],
+      expose: ['Authorization'],
+      max_age: 600
   end
-  
+end
