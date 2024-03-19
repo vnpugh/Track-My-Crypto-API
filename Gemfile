f96783681382194bd3c104a1e1614c9e8c167761
vnpugh <vnp19@yahoo.com>
2024-03-19 02:06:44 -0500

    updated API info

--- Gemfile
+++ Gemfile
@@ -41,6 +41,10 @@ gem 'jwt'
 # CORS
 gem 'rack-cors'
 
+# Interacts with external API
+gem 'faraday'
+
+
 # Use Active Model has_secure_password [https://guides.rubyonrails.org/active_model_basics.html#securepassword]
 gem 'bcrypt', '~> 3.1.7'
 
--- app/controllers/cryptos_controller.rb
+++ app/controllers/cryptos_controller.rb
@@ -2,43 +2,39 @@ class CryptosController < ApplicationController
     # This controller is used to fetch data from Coingecko API
 
     BASE_URI = 'https://api.coingecko.com/api/v3'
-    
 
-    # GET http://localhost:3000/cryptos/simple_price?ids=bitcoin,ethereum&vs_currencies=usd
+  
     def simple_price
-   
-        ids = params[:ids] # a comma-separated list of coin ids (e.g., 'bitcoin,ethereum')
-        vs_currencies = params[:vs_currencies] # target currencies
-        response = fetch_data("#{BASE_URI}/simple/price?ids=#{ids}&vs_currencies=#{vs_currencies}")
-        render json: response
- 
+      ids = params[:ids] 
+      vs_currencies = params[:vs_currencies] 
+      response = fetch_data_with_faraday("#{BASE_URI}/simple/price?ids=#{ids}&vs_currencies=#{vs_currencies}")
+      render json: response
     end
   
-    # GET   http://localhost:3000/cryptos/coins/markets?ids=bitcoin&vs_currency=usd
     def markets
       ids = params[:ids]
       vs_currency = params[:vs_currency]
-      response = fetch_data("#{BASE_URI}/coins/markets?vs_currency=#{vs_currency}&ids=#{ids}")
+      response = fetch_data_with_faraday("#{BASE_URI}/coins/markets?vs_currency=#{vs_currency}&ids=#{ids}")
       render json: response
     end
-
-    # GET http://localhost:3000/cryptos/coins/{id}/history
+  
     def historical_data
       id = params[:id]
       date = params[:date]
-      response = fetch_data("#{BASE_URI}/coins/#{id}/history?date=#{date}")
+      response = fetch_data_with_faraday("#{BASE_URI}/coins/#{id}/history?date=#{date}")
       render json: response
     end
-
   
-
     private
-
-    def fetch_data(url)
-      uri = URI(url)
-      response = Net::HTTP.get_response(uri)
-      return JSON.parse(response.body) if response.is_a?(Net::HTTPSuccess)
-    end
-
   
-end
+    def fetch_data_with_faraday(url)
+      response = Faraday.get(url)
+      if response.status == 200
+        JSON.parse(response.body)
+      else
+        { error: "Failed to fetch data" }
+      end
+    rescue Faraday::Error => e
+      { error: e.message }
+    end
+  end
